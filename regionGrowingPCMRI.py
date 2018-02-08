d0371ee05fcbc97adf951bf0b8924dc97b757d39
pueponey@gmail.com <so7Rooca>
2018-02-08 12:18:09 +0100

    skickit ok

--- regionGrowingPCMRI.py
+++ regionGrowingPCMRI.py
@@ -1,29 +1,3 @@
-"""
-Region Growing Segmentation
-===========================
-
-Thresholding is the most basic form of segmentation. The first step of
-improvement upon the naive thresholding is a class of algorithms called
-region growing.
-
-The common theme in this class of algorithms is that a voxel's
-neighbor is considered to be in the same class if its intensities are
-similar to the current voxel. The definition of similar is what varies.
-Initial set of voxel are called seed points. These initial seed points are
-usually manually selected.
-
-We illustrate the use of three variants of this family of algorithms:
-
-- `ConnectedThreshold <http://www.itk.org/Doxygen/html/classitk_1_1ConnectedThresholdImageFilter.html>`_
-- `ConfidenceConnected <http://www.itk.org/Doxygen/html/classitk_1_1ConfidenceConnectedImageFilter.html>`_
-- `VectorConfidenceConnected <http://www.itk.org/Doxygen/html/classitk_1_1VectorConfidenceConnectedImageFilter.html>`_
-
-We will illustrate the usage of these three filters using a cranial MRI
-scan (T1 and T2) and attempt to segment one of the ventricles.
-"""
-# License: CC-BY
-# sphinx_gallery_thumbnail_number = 4
-
 import os
 from read_click import getQ
 import numpy as np
@@ -35,13 +9,14 @@ from scipy.interpolate import interp1d
 from scipy.signal import savgol_filter
 import seaborn as sns
 
+# source for profiles doi/10.1111/echo.12994/full
 r = 3  # scaling factor
 multi = 7.5  # multiplicator for region growing
 
-extract = 1 #do not extract data from image but load npy array
+extract = 1  # do not extract data from image but load npy array
 
 if extract:
-    im = "/home/florian/liverSim/images/PCMRI/De lima Mendes/irm flux preop/QRURUMC4/KPBA3L5B/I2000001"
+    # im = "/home/florian/liverSim/images/PCMRI/De lima Mendes/irm flux preop/QRURUMC4/KPBA3L5B/I2000001"
     imfolder = '/home/florian/liverSim/images/PCMRI/De lima Mendes/irm flux preop/QRURUMC4/SCZ2RGG5/'
     imlist = glob.glob(imfolder + '*')
     imlist.sort()
@@ -88,14 +63,14 @@ if extract:
 
     k = 0
     q = np.empty([0])  # flow rate array
-    uavg = np.empty([0]) # avg veloc array
+    uavg = np.empty([0])  # avg veloc array
     for i in imlist:
         try:
             img = sitk.ReadImage(i)
             img = sitk.ReadImage(i)
             img = sitk.Expand(img, [r, r, 1] * 3, sitk.sitkLinear)  # resampling with same dx dy as for getQ function
             nda = sitk.GetArrayFromImage(img)
-            vesselArray[:, :, k] = nda[0, :, :] * segArray #get only the ROI
+            vesselArray[:, :, k] = nda[0, :, :] * segArray  # get only the ROI
             vesselArray[:, :, k] = vesselArray[:, :, k] * int(rescaleSlope) + int(rescaleIntercept)
             q = np.append(q, (np.sum(vesselArray[:, :, k])))
             uavg = np.append(uavg, (np.mean(vesselArray[:, :, k])))
@@ -108,18 +83,19 @@ if extract:
 else:
     uavg = np.load('veloc.npy')
 
-CP = 1 # heart period 1s
+CP = 1  # heart period 1s
 k = 8
-RP = k * CP # respiratory period 8s
+RP = k * CP  # respiratory period 8s
 
 np.save('veloc', uavg)
 
-#CORRECTION POUR CE CAS UNIQUEMENT : SLIDE #1 = pourrite
-uavg[0] = 0.5*(uavg[1]+uavg[-1])
+# CORRECTION POUR CE CAS UNIQUEMENT : SLIDE #1 = pourrite
+uavg[0] = 0.5 * (uavg[1] + uavg[-1])
 
 sns.set()
 sns.set_style("whitegrid")
 sns.despine(left=True)
+
 
 def smooth(Y):
     plotvar = np.append(np.append(Y, Y), Y)
@@ -129,6 +105,7 @@ def smooth(Y):
     xnew = np.linspace(0, 3 * CP, num=100, endpoint=True)
     plt.plot(x, plotvar, 'o', xnew, f3(xnew), '--')
     return f3
+
 
 vsmooth = smooth(uavg)
 print vsmooth
